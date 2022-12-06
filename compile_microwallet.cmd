6acf04ff2948bcd0ba91797a23aa642b033c84c0
hclivess <admin@bismuth.cz>
2022-12-06 18:48:55 +0100

    update compile script

--- compile_microwallet.cmd
+++ compile_microwallet.cmd
@@ -1,4 +1,4 @@
-python -m nuitka microwallet.py --follow-imports --include-data-files="C:/Program Files/Python38/Lib/site-packages/customtkinter/*.*=customtkinter/" --windows-icon-from-ico=graphics\icon.ico --enable-plugin=tk-inter
+python -m nuitka microwallet.py --follow-imports --include-data-files="C:/Program Files/Python310/Lib/site-packages/customtkinter/*.*=customtkinter/" --windows-icon-from-ico=graphics\icon.ico --enable-plugin=tk-inter
 xcopy /e /i /y sources\customtkinter microwallet.dist\customtkinter
 xcopy /e /i /y peers microwallet.dist\peers
 "C:\Program Files (x86)\Inno Setup 6\iscc" /q "setup.iss"
