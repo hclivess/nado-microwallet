a8fcf6caaab0828c84eda025fbc2bc8338112c65
hclivess <admin@bismuth.cz>
2022-12-18 19:29:17 +0100

    fix compiler

--- compile_microwallet.cmd
+++ compile_microwallet.cmd
@@ -1,5 +1,5 @@
-rmdir /s /q nado.build
-rmdir /s /q nado.dist
+rmdir /s /q microwallet.build
+rmdir /s /q microwallet.dist
 python -m nuitka microwallet.py --follow-imports --include-data-files="C:/Program Files/Python310/Lib/site-packages/customtkinter/*.*=customtkinter/" --windows-icon-from-ico=graphics\icon.ico --enable-plugin=tk-inter
 xcopy /e /i /y sources\customtkinter microwallet.dist\customtkinter
 "C:\Program Files (x86)\Inno Setup 6\iscc" /q "setup.iss"
