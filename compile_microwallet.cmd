212d7ceedba01b11191823f41b4c3fd3d3846686
hclivess <admin@bismuth.cz>
2022-12-18 19:42:29 +0100

    proper compilation

--- compile_microwallet.cmd
+++ compile_microwallet.cmd
@@ -1,6 +1,6 @@
 rmdir /s /q microwallet.build
 rmdir /s /q microwallet.dist
-python -m nuitka microwallet.py --standalone --enable-plugin=numpy --enable-plugin=numpy --include-data-files="C:/Program Files/Python310/Lib/site-packages/customtkinter/*.*=customtkinter/" --windows-icon-from-ico=graphics\icon.ico --enable-plugin=tk-inter
+python -m nuitka microwallet.py --standalone --enable-plugin=numpy --enable-plugin=pyside2 --include-data-files="C:/Program Files/Python310/Lib/site-packages/customtkinter/*.*=customtkinter/" --windows-icon-from-ico=graphics\icon.ico --enable-plugin=tk-inter
 xcopy /e /i /y sources\customtkinter microwallet.dist\customtkinter
 "C:\Program Files (x86)\Inno Setup 6\iscc" /q "setup.iss"
 pause
