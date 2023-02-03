beca9963dc2fc73affe49d185085cf8e2906d1d8
hclivess <admin@bismuth.cz>
2023-02-03 23:28:58 +0100

    make life for devs easier by not packing tx submission errors

--- compounder.py
+++ compounder.py
@@ -73,7 +73,7 @@ async def send_transaction(peer, port, logger, fail_storage, transaction, compre
         async with sem:
             http_client = AsyncHTTPClient()
             response = await http_client.fetch(url_construct, request_timeout=5)
-            fetched = msgpack.unpackb(response.body)["message"]
+            fetched = json.loads(response.body)["message"]
             return peer, fetched
 
     except Exception as e:
